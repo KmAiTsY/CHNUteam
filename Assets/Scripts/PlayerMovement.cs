90be42f80b2ad49a5644068a272aabc0e08ec1cb
KmAiTsY <mishagandabura@gmail.com>
2021-05-24 20:17:45 +0300

    init commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public int speed;
+    public int force;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            rigidbody2D.AddForce(transform.up * force, ForceMode2D.Impulse);
+        }
+    }
+}
