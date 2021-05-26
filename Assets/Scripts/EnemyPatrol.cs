5370e4ad14a3a3b0026abf9a20f7f943cac73d0d
KmAiTsY <mishagandabura@gmail.com>
2021-05-26 16:49:23 +0300

    New player script and edit scene

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public SpriteRenderer spriteRenderer;
+    public GameObject leftBorder;
+    public GameObject rightBorder;
+    public bool isRightDirection;
+    public float speed;
+    void Update()
+    {
+        if(isRightDirection)
+        {
+            /*rigidbody.velocity = new Vector2(1 * speed, 0);*/
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            if (transform.position.x > rightBorder.transform.position.x)
+            {
+                isRightDirection = !isRightDirection;
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+            }
+        }
+        else
+        {
+            /*rigidbody.velocity = new Vector2(-1 * speed, 0);*/
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            if (transform.position.x < leftBorder.transform.position.x)
+            {
+                isRightDirection = !isRightDirection;
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform transform;
+    public Rigidbody2D rigidbody;
+    public SpriteRenderer spriteRenderer;
+    public BoxCollider2D boxCollider;
+    public GameObject leftButton;
+    public GameObject rightButton;
+    public float speed;
+    public float force;
+    public float minimalHeight;
+    public bool isCheatMode;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            spriteRenderer.flipX = true;
+            boxCollider.offset = new Vector2(1.565558f, -0.8419857f);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            spriteRenderer.flipX = false;
+            boxCollider.offset = new Vector2(-1.565558f, -0.8419857f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            rigidbody.AddForce(transform.up * force, ForceMode2D.Impulse);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.R) || transform.position.y < minimalHeight) && isCheatMode)
+        {
+            rigidbody.velocity = new Vector2(0, 0);
+            transform.position = new Vector2(0, 0);
+        }
+        else if (transform.position.y < minimalHeight && !isCheatMode)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
